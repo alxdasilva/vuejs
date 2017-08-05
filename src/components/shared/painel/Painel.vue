89eee9be9b6252e32387f518c99a6bf3ea48f84b
Alex <alx.silva@live.com>
2017-08-05 17:14:03 -0300

    alurapic

---
+++ src/components/shared/painel/Painel.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="painel">
+    <h2 @dblclick="visivel = !visivel" class="painel-titulo">{{ titulo }}</h2>
+    <transition name="painel-fade">
+      <div @dblclick="visivel = !visivel" class="painel-conteudo" v-show="visivel" >
+        <slot></slot>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      visivel: true
+    }
+
+  },
+  props: ['titulo']
+}
+</script>
+
+<style scoped>
+/* estilo painel*/
+.painel {
+  padding: 0 auto;
+  border: solid 2px grey;
+  display: inline-block;
+  margin: 5px;
+  box-shadow: 5px 5px 10px grey;
+  width: 200px;
+  height: 100%;
+  vertical-align: top;
+  text-align: center;
+}
+
+.painel .painel-titulo {
+  text-align: center;
+  border: solid 2px;
+  background: lightblue;
+  margin: 0 0 15px 0;
+  padding: 10px;
+  text-transform: uppercase;
+}
+* {
+  box-shadow: 5px 5px 10px;
+}
+.painel-fade-enter, .painel-fade-leave-active {
+  opacity: 0;
+}
+.painel-fade-enter-active, .painel-fade-leave-active {
+  transition: opacity .4s;
+}
+</style>
